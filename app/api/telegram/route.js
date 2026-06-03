31775e1a8789e8984dec388f1c0892f676e1ec83
stopboncos <stopboncos@gmail.com>
2026-06-03 13:26:20 +0800

    503

--- app/api/telegram/route.js
+++ app/api/telegram/route.js
@@ -2,7 +2,7 @@ import { createClient } from '@supabase/supabase-js'
 
 const supabase = createClient(
   process.env.NEXT_PUBLIC_SUPABASE_URL,
-  process.env.SUPABASE_SECRET_KEY
+  process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY
 )
 
 const TOKEN = process.env.TELEGRAM_BOT_TOKEN
